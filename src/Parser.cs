0cba9c4bc7efaf8ce7de568e23482e103fdb78f5
Atif Aziz <code@raboof.com>
2018-06-08 16:56:01 +0200

    Reivew TextRow field finding methods for consistency

--- src/Parser.cs
+++ src/Parser.cs
@@ -106,19 +106,26 @@ namespace Dsv
 
     public static class TextRowExtensions
     {
+        // Methods that get the index of first match; otherwise throw
+
+        public static int GetFirstIndex(this TextRow row, string sought) =>
+            row.GetFirstIndex(sought, StringComparison.Ordinal);
+
+        public static int GetFirstIndex(this TextRow row,
+            string sought, StringComparison comparison) =>
+            row.GetFirstIndex(s => string.Equals(s, sought, comparison));
+
+        public static int GetFirstIndex(this TextRow row, Func<string, bool> predicate) =>
+            row.FindFirstIndex(predicate) is int i ? i : throw new InvalidOperationException("Sequence contains no elements.");
+
+        // Methods that get the index of first match; otherwise return null
+
         public static int? FindFirstIndex(this TextRow row, string sought) =>
             row.FindFirstIndex(sought, StringComparison.Ordinal);
-
-        public static IEnumerable<int> FindIndex(this TextRow row, string sought) =>
-            row.FindIndex(sought, StringComparison.Ordinal);
 
         public static int? FindFirstIndex(this TextRow row,
             string sought, StringComparison comparison) =>
             row.FindFirstIndex(s => string.Equals(s, sought, comparison));
-
-        public static IEnumerable<int> FindIndex(this TextRow row,
-            string sought, StringComparison comparison) =>
-            row.Find(sought, comparison, (_, i) => i);
 
         public static int? FindFirstIndex(this TextRow row, Func<string, bool> predicate)
         {
@@ -131,64 +138,116 @@ namespace Dsv
             return null;
         }
 
+        // Method to find index of fields matching a condition
+
         public static IEnumerable<int> FindIndex(this TextRow row, Func<string, bool> predicate) =>
             row.Find((s, i) => predicate(s) is bool matched && matched
                              ? (true, i) : (false, default));
 
-        public static IEnumerable<(string Field, int Index)>
-            Find(this TextRow row, string sought) =>
-            Find(row, sought, StringComparison.Ordinal);
+        // Methods to find the first field matching a condition; otherwise
+        // return an alternative when none match.
 
-        public static IEnumerable<(string Field, int Index)>
-            Find(this TextRow row, string sought, StringComparison comparison) =>
-            row.Find(sought, comparison, ValueTuple.Create);
+        public static T FindFirstField<T>(this TextRow row, string sought,
+            Func<string, int, T> someSelector) =>
+            row.FindFirstField(sought, someSelector, default(T));
 
-        public static IEnumerable<T> Find<T>(this TextRow row,
-            string sought, Func<string, int, T> resultSelector) =>
-            Find(row, sought, StringComparison.Ordinal, resultSelector);
+        public static T FindFirstField<T>(this TextRow row, string sought,
+            Func<string, int, T> someSelector, T none) =>
+            row.FindFirstField(sought, someSelector, () => none);
 
-        public static IEnumerable<T> Find<T>(this TextRow row,
-            string sought, StringComparison comparison, Func<string, int, T> resultSelector) =>
-            row.Find((s, i) => string.Equals(sought, s, comparison) is bool matched && matched
-                             ? (true, resultSelector(s, i))
-                             : default);
+        public static T FindFirstField<T>(this TextRow row, string sought,
+            Func<string, int, T> someSelector, Func<T> noneSelector) =>
+            row.FindFirstField(sought, StringComparison.Ordinal, someSelector, noneSelector);
 
-        public static IEnumerable<(string Field, int Index)>
-            Find(this TextRow row, Func<string, int, bool> predicate) =>
-            row.Find((s, i) => predicate(s, i) ? (true, (s, i)) : default);
+        public static T FindFirstField<T>(this TextRow row,
+            string sought, StringComparison comparison,
+            Func<string, int, T> someSelector) =>
+            row.FindFirstField(sought, comparison, someSelector, default(T));
 
-        public static IEnumerable<T> Find<T>(this TextRow row,
-            Func<string, int, (bool, T)> predicate)
+        public static T FindFirstField<T>(this TextRow row,
+            string sought, StringComparison comparison,
+            Func<string, int, T> someSelector, T none) =>
+            row.FindFirstField(sought, comparison, someSelector, () => none);
+
+        public static T FindFirstField<T>(this TextRow row,
+            string sought, StringComparison comparison,
+            Func<string, int, T> someSelector, Func<T> noneSelector)
         {
-            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
-
-            return _(); IEnumerable<T> _()
+            using (var e = row.Find((s, i) => string.Equals(sought, s, comparison) is bool matched
+                                              && matched
+                                            ? (true, someSelector(s, i))
+                                            : default).GetEnumerator())
             {
-                for (var i = 0; i < row.Count; i++)
-                {
-                    var (matched, result) = predicate(row[i], i);
-                    if (matched)
-                        yield return result;
-                }
+                return e.MoveNext() ? e.Current : noneSelector();
             }
         }
 
-        public static (string Field, int Index)
-            MatchFirst(this TextRow row, string pattern) =>
-            MatchFirst(row, pattern, RegexOptions.None);
+        // Methods to get the first matching field; otherwise throw
+
+        public static (string Field, int Index) GetFirstField(this TextRow row,
+            string sought, StringComparison comparison) =>
+            row.GetFirstField(sought, comparison, ValueTuple.Create);
+
+        public static T GetFirstField<T>(this TextRow row,
+            string sought, StringComparison comparison, Func<string, int, T> resultSelector) =>
+            row.Find((s, i) => string.Equals(sought, s, comparison) is bool matched && matched
+                             ? (true, resultSelector(s, i))
+                             : default)
+               .First();
+
+        // Methods that find the first field matching a pattern; otherwise
+        // return an alternative when none match.
+
+        public static T FindFirstMatch<T>(this TextRow row,
+            string pattern, Func<string, int, Match, T> someSelector) =>
+            row.FindFirstMatch(pattern, someSelector, default(T));
+
+        public static T FindFirstMatch<T>(this TextRow row,
+            string pattern, Func<string, int, Match, T> someSelector, T none) =>
+            row.FindFirstMatch(pattern, RegexOptions.None, someSelector, none);
+
+        public static T FindFirstMatch<T>(this TextRow row,
+            string pattern, RegexOptions options,
+            Func<string, int, Match, T> someSelector) =>
+            row.FindFirstMatch(pattern, options, someSelector, default(T));
+
+        public static T FindFirstMatch<T>(this TextRow row,
+            string pattern, RegexOptions options,
+            Func<string, int, Match, T> someSelector, T none) =>
+            row.FindFirstMatch(pattern, options, someSelector, () => none);
+
+        public static T FindFirstMatch<T>(this TextRow row, string pattern,
+            Func<string, int, Match, T> someSelector, Func<T> noneSelector) =>
+            row.FindFirstMatch(pattern, RegexOptions.None, someSelector, noneSelector);
+
+        public static T FindFirstMatch<T>(this TextRow row,
+            string pattern, RegexOptions options,
+            Func<string, int, Match, T> someSelector, Func<T> noneSelector)
+        {
+            using (var e = row.Match(pattern, options, someSelector).GetEnumerator())
+                return e.MoveNext() ? e.Current : noneSelector();
+        }
+
+        // Methods that get the first field matching a pattern; otherwise throw
 
         public static (string Field, int Index)
-            MatchFirst(this TextRow row, string pattern, RegexOptions options) =>
-            row.MatchFirst(pattern, options, (s, i, _) => (s, i));
+            GetFirstMatch(this TextRow row, string pattern) =>
+            GetFirstMatch(row, pattern, RegexOptions.None);
 
-        public static T MatchFirst<T>(this TextRow row,
+        public static (string Field, int Index)
+            GetFirstMatch(this TextRow row, string pattern, RegexOptions options) =>
+            row.GetFirstMatch(pattern, options, (s, i, _) => (s, i));
+
+        public static T GetFirstMatch<T>(this TextRow row,
             string pattern, Func<string, int, Match, T> resultSelector) =>
-            row.Match(pattern, resultSelector).First();
+            row.GetFirstMatch(pattern, RegexOptions.None, resultSelector);
 
-        public static T MatchFirst<T>(this TextRow row,
+        public static T GetFirstMatch<T>(this TextRow row,
             string pattern, RegexOptions options,
             Func<string, int, Match, T> resultSelector) =>
             row.Match(pattern, options, resultSelector).First();
+
+        // Methods that all fields matching a pattern
 
         public static IEnumerable<(string Field, int Index)>
             Match(this TextRow row, string pattern) =>
@@ -211,6 +270,28 @@ namespace Dsv
                 return (m.Success, (Input: s, Index: i, Match: m));
             })
             select resultSelector(e.Input, e.Index, e.Match);
+
+        // Methods that find all fields matching a condition
+
+        public static IEnumerable<(string Field, int Index)>
+            Find(this TextRow row, Func<string, int, bool> predicate) =>
+            row.Find((s, i) => predicate(s, i) ? (true, (s, i)) : default);
+
+        public static IEnumerable<T> Find<T>(this TextRow row,
+            Func<string, int, (bool, T)> predicate)
+        {
+            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+
+            return _(); IEnumerable<T> _()
+            {
+                for (var i = 0; i < row.Count; i++)
+                {
+                    var (matched, result) = predicate(row[i], i);
+                    if (matched)
+                        yield return result;
+                }
+            }
+        }
     }
 
     static partial class Parser
